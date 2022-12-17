4c0cd3619a7c11e501b2d6d5c764d85c16f74b2c
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-17 11:36:59 +0100

    feat: Arithmetic Operators

---
+++ operators/arithmetic_operator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // int a, b, c;
+    float a, b, c;
+
+    printf("Enter A operand (float): ");
+    scanf("%f", &a);
+
+    printf("Enter B operand (float): ");
+    scanf("%f", &b);
+    printf("\n");
+
+    c = a + b;
+    printf("c = a + b --> %9.3f\n", c);
+
+    c = a - b;
+    printf("c = a - b --> %9.3f\n", c);
+
+    c = a * b;
+    printf("c = a * b --> %9.3f\n", c);
+
+    c = a / b;
+    printf("c = a / b --> %9.3f\n", c);
+
+    // c = a % b;
+    // printf("c = a %% b --> %d\n", c);
+
+
+    printf("\n\n=== ByteGarage ===\n\n");
+    return EXIT_SUCCESS;
+}
