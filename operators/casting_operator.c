3d15ecc8e564f0eca0cb7c2bd5a6828256a88816
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-19 11:48:09 +0100

    feat:Casting Operator

---
+++ operators/casting_operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int intNumber = (int) 7.65;
+
+    printf("Casted int = %d\n", intNumber);
+     
+    printf("\n\n=== ByteGarage ===\n\n");
+    return EXIT_SUCCESS;
+}
