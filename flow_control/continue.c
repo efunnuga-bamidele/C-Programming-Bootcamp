f02bf54aee08503b6465131531e742a0ccc696eb
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-21 12:27:27 +0100

    feat:Continue in loop

---
+++ flow_control/continue.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int input, accumulator = 0;
+    const int ITERATION = 5;
+
+    for (int i = 0; i < ITERATION; i++)
+    {
+        printf("Enter an integer (> 0): ");
+        scanf("%d", &input);
+
+        if(input < 0)
+        {
+            continue;
+        }
+
+        accumulator += input;
+    }
+
+    printf("\nAccumulator: %d\n", accumulator);
+
+    printf("\n\n=== ByteGarage ===\n\n");
+    return EXIT_SUCCESS;
+}
