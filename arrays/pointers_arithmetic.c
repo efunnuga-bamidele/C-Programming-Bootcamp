99c897dfb66a7251e6c4786c8bff1c474281bd9f
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-27 14:30:07 +0100

    feat:Pointer Arithmetic

---
+++ arrays/pointers_arithmetic.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define X_LEN 10
+
+int sum_arr(int arr[], const int len)
+{
+    int sum = 0;
+    for (int i = 0; i < len; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int sum_ptr(const int *const p, const int len)
+{
+    int sum = 0;
+    for (int i = 0; i < len; i++)
+    {
+        sum += p[i];
+    }
+
+    return sum;
+}
+
+int sum_ptr_arithmetic(const int *const p, const int len)
+{
+    int sum = 0;
+    for (const int *p_item = p; p_item < p + len; p_item++)
+    {
+        sum += *p_item;
+        printf("%lld %lld, %d\n", p_item - p, (int64_t)p_item, sum);
+    }
+    return sum;
+}
+
+int main()
+{
+    printf("\n\n=== Pointers Arithmetic ===\n\n");
+
+    int x[]= {1, 4, -3, 2};
+
+    printf("sum_arr: %d\n", sum_arr(x, X_LEN));
+    printf("sum_ptr: %d\n", sum_ptr(x, X_LEN));
+
+    printf("-----\n");
+    int *ptr = x;
+    printf("%lld, %d\n", (uint64_t) ptr, *ptr);
+    ptr++;
+    printf("%lld, %d\n", (uint64_t) ptr, *ptr);
+
+    printf("-----\n");
+    printf("sum_ptr_arithmetic: %d\n", sum_ptr_arithmetic(x, X_LEN));
+
+
+    printf("\n\n=== ByteGarage ===\n\n");
+    return EXIT_SUCCESS;
+}
