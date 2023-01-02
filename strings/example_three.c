8f84ae46ac0b1f505966ca27dfec15956cf867a5
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-01-02 11:12:52 +0100

    feat:String Copy Function

--- strings/example_three.c
+++ strings/example_three.c
@@ -7,11 +7,11 @@ int main()
     printf("\n\n=== Strig Methods {Length, Copy, Compare} ===\n\n");
 
     // Receive user Input
-    char input_str[10];
-    printf("Enter a string of (9 characters): ");
-    scanf("%s", &input_str);
+    // char input_str[10];
+    // printf("Enter a string of (9 characters): ");
+    // scanf("%s", &input_str);
 
-    printf("%s\n", input_str);
+    // printf("%s\n", input_str);
 
 
     // String Length
@@ -26,6 +26,17 @@ int main()
     
     //String length: Built in Method 
     printf("String length (built-in): %lld\n", strlen(my_string));
+
+    // String Copy: strcpy, strncpy
+    char another_string[] = "Programming is fun!";
+    char another_long_string[] = "This is a dummy very very long string.......";
+    printf("Before copy: \"%s\"\n", my_string);
+    printf("After copy using (STRCPY): \"%s\"\n", strcpy(my_string, another_string)); //does not check the max number of element to copy in destination string
+    
+    printf("\n === \n\n");    
+    printf("Before copy: \"%s\"\n", my_string);
+    printf("After copy using (STRNCPY): \"%s\"\n", strncpy(my_string, another_long_string, sizeof(another_long_string))); 
+
 
 
     printf("\n\n=== ByteGarage ===\n\n");
