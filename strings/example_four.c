79e8e611a275ff93567bf2cea7d31a00e4be5bbe
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-01-03 12:10:30 +0100

    feat:Tokenize a String into words

--- strings/example_four.c
+++ strings/example_four.c
@@ -57,12 +57,38 @@ void print_substring(char str[], char sub[], int index)
     }
 }
 
+int tokenize_string(char str[], char delimiters[],int tok_max_len,  char tokens[][tok_max_len])
+{
+    char *ptok = str;
+    int i;
+
+    for (i = 0; ptok; i++)
+    {
+        ptok = strtok(i == 0 ? ptok : NULL, delimiters);
+
+        strncpy(tokens[i], ptok, ptok ? tok_max_len : 0);
+    }
+    return i-1;
+}
+
+void print_tokens(int n_tokens, int tok_max_len, char tokens[][tok_max_len], char str[], char delimiters[])
+{
+    printf("\n\nTokenization with delimiters \"%s\"\n", delimiters);
+    printf("%s\n", str);
+
+    for (int i = 0; i < n_tokens; i++)
+    {
+        printf("[%d] %s\n", i, tokens[i]);
+    }
+    
+}
+
 int main()
 {
 
     printf("\n\n=== String Search and Tokenization ===\n\n");   
 
-    char str[] = "This is a sample string";
+    char str[] = "This, is a. sample-string";
     int occurances_indexes[sizeof(str)];
     int found = find_all_occurances(str, 'i', occurances_indexes);
 
@@ -72,6 +98,13 @@ int main()
     int index = find_substring(str, substring);
     print_substring(str, substring, index);
 
-    printf("\n\n=== ByteGarage ===\n\n");
+    char delimiters[] =" ,.-";
+    int tok_max_len = sizeof(str);
+    char tokens[tok_max_len][tok_max_len], str_cpy[tok_max_len];
+    strncpy(str_cpy, str, tok_max_len);
+    int n_tokens = tokenize_string(str_cpy, delimiters, tok_max_len, tokens);
+    print_tokens(n_tokens, tok_max_len, tokens, str, delimiters);
+
+    printf("\n\n=== ByteGarage ===\n\n"); 
     return EXIT_SUCCESS;
 }
