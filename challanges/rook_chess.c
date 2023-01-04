e42e0afd9b4c25b550b512791c1bb05cb7313fb6
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-01-04 12:38:05 +0100

    feat:Rook Chess Moves Challenge

---
+++ challanges/rook_chess.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void rook_move(char current_position[], char output_buffer[])
+{
+    const char cols[] = "abcdefgh";
+    const char rows[] = "12345678";
+
+    char curr_col = current_position[0];
+    char curr_row = current_position[1];
+    char *dst = output_buffer;
+
+    for (const char *p = cols; *p != '\0'; p++ )
+    {
+        if (*p != curr_col)
+        {
+            *dst++ = *p;
+            *dst++ = curr_row;
+            *dst++ = ' ';
+        }
+
+    }
+
+    for (const char *p = rows; *p != '\0'; p++ )
+    {
+        if (*p != curr_row)
+        {
+            *dst++ = curr_col;
+            *dst++ = *p;
+            *dst++ = ' ';
+        }
+        
+    }
+
+    *dst = '\0';
+
+}
+
+int main()
+{
+
+    printf("\n\n=== Move The Rook! ===\n\n");
+    
+    char pos[] = "d5";
+    char buffer[100];
+
+    rook_move(pos, buffer);
+    printf("Current position: %s\n", pos);
+    printf("You can move to: %s\n\n", buffer);
+
+    printf("\n\n=== ByteGarage ===\n\n");
+    return EXIT_SUCCESS;
+}
