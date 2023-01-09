8b1f7bcc0b88e50c82220d0c0f757b4040b35a3c
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-01-09 11:53:22 +0100

    feat:Memory Allocation

---
+++ dynamic_memory_allocation/example_one.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+
+/**
+ * Allocate memory -> void *malloc(size_t size)
+ * Allocate array of elements -> void *calloc(size_t num, size_t size)
+ * Reallocate memory -> void *realloc(void *ptr, size_t new_size)
+ * Free memory -> void *free(void *ptr)
+*/
+
+int main()
+{
+    printf("\n\n=== Get The Line Dynamically ===\n\n");
+
+
+    printf("\n\n=== ByteGarage ===\n\n");
+    return EXIT_SUCCESS;
+}
